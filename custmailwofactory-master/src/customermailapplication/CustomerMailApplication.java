34ff3af825f700eae7a3268686c6e321afc095a5
วรายุทธ พรมพาดี <87457264+cskmitl@users.noreply.github.com>
2023-08-10 21:20:47 +0700

    Add files via upload

---
+++ custmailwofactory-master/src/customermailapplication/CustomerMailApplication.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package customermailapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sarun
+ */
+public class CustomerMailApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    private Customer customer;
+
+    public void getCustomerTypeFromUser(String customerType) {
+        customer = CustomerFactory.createCustomer(customerType);
+    }
+
+    public String generateMail() {
+        return customer.createMail();
+    }
+
+    public static void main(String[] args) {
+        CustomerMailApplication app = new CustomerMailApplication();
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Please choose customer type: 1. Regular, 2. Mountain, 3. Delinquent ");
+        int type = inp.nextInt();
+
+        switch (type) {
+            case 1:
+                app.getCustomerTypeFromUser("Regular");
+                break;
+            case 2:
+                app.getCustomerTypeFromUser("Mountain");
+                break;
+            case 3:
+                app.getCustomerTypeFromUser("Delinquent");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                return;
+        }
+
+        System.out.println(app.generateMail());
+    }
+}
